13effda895478be47b99c94c4b1cefcd98ac3416
OvidioCalvet <calvetovidio@gmail.com>
2024-11-06 12:51:18 -0500

    added some logic and calculations

--- main.py
+++ main.py
@@ -3,36 +3,84 @@ import random
 cards = [11,1,2,3,4,5,6,7,8,9,10,10,10]
 
 user_hand = []
+user_hand_amount = 0
+user_hand_status = ''
 dealer_hand = []
+dealer_hand_amount = 0
+dealer_hand_status = ''
 
-def deal(player):
-    for i in range(0,2): player.append(random.choice(cards))
-    return player
+def deal(player_hand):
 
-def hit(player):
-    player.append(random.choice(cards))
-    return player
+    for i in range(0,2): player_hand.append(random.choice(cards))
+    return player_hand
+
+def hit(player_hand): 
+    
+    player_hand.append(random.choice(cards))
+    return player_hand
+
+def calculate(player_hand):
+
+    player_hand_amount = 0  
+
+    for i in range(0, len(player_hand)): player_hand_amount = player_hand_amount + player_hand[i]
+    return player_hand_amount
+
+
 
 user_hand = deal(user_hand)
 dealer_hand = deal(dealer_hand)
 
-game_is_not_over = True
+print(f'Player hand: {user_hand}')
+print(f'Dealer hand: [{dealer_hand[1]}]\n')
 
-while game_is_not_over:
+user_hand_is_not_over = True
+
+while user_hand_is_not_over:
 
     choice = input(f'Your hand is {user_hand}, do you want to hit or stand: ')
 
-    user_hand = hit(user_hand)
+    if choice == 'hit':
 
-    hand_amount = 0
+        user_hand = hit(user_hand)
+        user_hand_amount = calculate(user_hand)
 
-    for i in range(0, len(user_hand)):
-        hand_amount = hand_amount + user_hand[i]
+        if user_hand_amount > 21:
 
-    print(user_hand)
-    print(hand_amount)
+            user_hand_status = 'bust'
+            user_hand_is_not_over = False
 
-    game_is_not_over = False
+            print('You busted! (pause lmao)')
+
+        elif user_hand_amount == 21:
+
+            user_hand_status = 'jackpot'
+            user_hand_is_not_over = False
+
+            print('You hit Jackpot! lets see what the dealer gets..')
+
+
+    else: 
+        user_hand_is_not_over = False
+
+        print('Lets see what the dealer gets..')
+
     
+print(f'Dealer reveals: {dealer_hand}')
+
+
+dealer_hand_is_not_over = True
+
+while dealer_hand_is_not_over:
+
+    dealer_hand_amount = calculate(dealer_hand)
+
+    if dealer_hand_amount >= 17 and dealer_hand_amount <= 21: dealer_hand_is_not_over = False
+
+    elif dealer_hand_amount < 17:
+
+        dealer_hand = hit(dealer_hand)
+        print(dealer_hand)
+
 
 
